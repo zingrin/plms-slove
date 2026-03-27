1872d9970a1263a3742f0f851f26daef67bd9c51
zingrin <jerinloncheu@gmail.com>
2026-03-27 14:48:00 +0600

    plms sloved

--- script.js
+++ script.js
@@ -90,3 +90,19 @@ function reverseString(str) {
 }
 
 console.log(reverseString("hello"));
+function findDuplicates(arr) {
+  let seen = new Set();
+  let duplicates = new Set();
+
+  for (let num of arr) {
+    if (seen.has(num)) {
+      duplicates.add(num);
+    } else {
+      seen.add(num);
+    }
+  }
+
+  return [...duplicates];
+}
+
+console.log(findDuplicates([1, 2, 3, 2, 4, 5, 1]));
