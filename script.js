adc9d88c98f37b8132305331e9c4853aa27e47fc
zingrin <jerinloncheu@gmail.com>
2026-03-28 15:36:44 +0600

    sloved added

--- script.js
+++ script.js
@@ -116,3 +116,22 @@ function secondLargest(arr) {
 }
 
 console.log(secondLargest([10, 5, 20, 8, 20]));
+
+// loop
+function secondLargest(arr) {
+  let largest = -Infinity;
+  let second = -Infinity;
+
+  for (let num of arr) {
+    if (num > largest) {
+      second = largest;
+      largest = num;
+    } else if (num > second && num !== largest) {
+      second = num;
+    }
+  }
+
+  return second;
+}
+
+console.log(secondLargest([10, 5, 20, 8, 20]));
