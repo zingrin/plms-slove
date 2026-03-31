c6a9316ebb5a34fbbda0c2735cd055c1f628c2e6
zingrin <jerinloncheu@gmail.com>
2026-03-31 16:11:50 +0600

    all done

--- problems.js
+++ problems.js
@@ -12,5 +12,35 @@ function twoSum(nums, target) {
     map[nums[index]] = index;
   }
 }
+// console.log(twoSum([2, 7, 11, 15], 9));
 
-console.log(twoSum([2, 7, 11, 15], 9));
+// numbers= [88,88,39,90,877,38];
+// target = 126;
+
+function sumT(numbers, t) {
+  const map = {};
+  for (let index = 0; index < numbers.length; index++) {
+    const element = t - numbers[index];
+
+    if (map[element] !== undefined) {
+      return [map[element], index];
+    }
+    map[numbers[index]] = index;
+  }
+}
+console.log(sumT([88, 88, 39, 877, 38], 126));
+// const nums1 = [10, 5, 8, 20, 15];
+// const target1 = 25;
+
+function tSum(nums1, tr) {
+  const map = {};
+  for (let index = 0; index < nums1.length; index++) {
+    const element = tr - nums1[index];
+    if (map[element] !== undefined) {
+      return [map[element], index];
+    }
+
+    map[nums1[index]] = index;
+  }
+}
+console.log(tSum([10, 5, 8, 20, 15], 25));
