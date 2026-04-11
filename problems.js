e8d58cc4c9a42a0fbdc98eefae60cf85ac4d8693
zingrin <jerinloncheu@gmail.com>
2026-04-11 21:28:12 +0600

    all done

--- problems.js
+++ problems.js
@@ -113,3 +113,20 @@ function firstUniqueChar(str) {
 }
 
 console.log(firstUniqueChar("programming"));
+function secondLargest(arr) {
+  let first = -Infinity;
+  let second = -Infinity;
+
+  for (let num of arr) {
+    if (num > first) {
+      second = first;
+      first = num;
+    } else if (num > second && num !== first) {
+      second = num;
+    }
+  }
+
+  return second;
+}
+
+console.log(secondLargest([10, 5, 20, 8])); // 10
