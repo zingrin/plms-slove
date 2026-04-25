29f992c86f9505f5861f183fe609b3ce3e4a8e08
zingrin <jerinloncheu@gmail.com>
2026-04-25 23:36:34 +0600

    all done

--- script.js
+++ script.js
@@ -179,3 +179,21 @@ const b = [3, 4];
 const merged = [...a, ...b];
 
 console.log(merged); // [1,2,3,4]
+function isAnagram(str1, str2) {
+  if (str1.length !== str2.length) return false;
+
+  let count = {};
+
+  for (let char of str1) {
+    count[char] = (count[char] || 0) + 1;
+  }
+
+  for (let char of str2) {
+    if (!count[char]) return false;
+    count[char]--;
+  }
+
+  return true;
+}
+
+console.log(isAnagram("listen", "silent")); // true
